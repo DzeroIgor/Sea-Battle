717f71c5cbeade6ccb17011362ab3ea1a22327f7
Igor Dzero <dzeroigor.dtp@gmail.com>
2024-01-21 12:35:30 +0200

    +

--- main.js
+++ main.js
@@ -53,19 +53,15 @@ let layers = [
 
 let seaDiv = document.querySelector('.sea')
 let renderDiv = document.querySelector('.render')
+let renderExplosionDiv = document.querySelector('.explosion-render')
 
 let timerShip
 let timerRochet
 let ridx
 let score = 0
+
 let layerWidth = innerWidth * (1 + 1.2)
-// console.log(layerW)
-
 let offset = 0.6 * innerWidth + 80
-// console.log(offset)
-
-// let l = document.querySelector('.layer')
-// const leftValue = window.getComputedStyle(l).left;
 
 document.addEventListener('DOMContentLoaded', function() {
   const l = document.querySelector('.layer');
@@ -77,11 +73,11 @@ let ship = {
     x: -600,
     dir: -1,
     layer: 1,
-    index: 20 - 1,
 }
 
 let scope = {
-    x: 0,}
+    x: 0,
+}
 
 let rocket = {
     x: 800,
@@ -108,10 +104,10 @@ const render = () => {
     for(let i = 0; i <=9; i++) {
 
         let objects = ``
-        
+                                                      // z-index: calc(19 - ${ship.layer});
         if(layers[i].ship) {
             objects += `
-            <div class="ship" style="left: ${ship.x}px; z-index: calc(19 - ${ship.layer});">
+            <div class="ship" style="left: ${ship.x}px; ">
                 <div class="hull">
                     <div class="gun">
                         <div class="tube"></div>
@@ -149,17 +145,7 @@ const render = () => {
             </div>            
             `
         }
-
-        if(layers[i].explosion) {             
-            objects += `
-                <div class="explosion" style="left: ${explosion.x}px;">
-                    <div class="fire-bottom-sm"></div>
-                    <div class="fire-bottom-md"></div>
-                    <div class="fire-bottom-lg"></div>
-                </div>
-            `;
-        }
-
+       
         renderDiv.innerHTML += `
         <div class="layer"> <!-- layer ${i} -->
             ${objects}            
@@ -176,11 +162,39 @@ const render = () => {
     `
 }
 
+const renderExplosion = () => {
+    
+    renderExplosionDiv.innerHTML = ``
+
+    for(let i = 0; i <=9; i++) {
+
+        let objects = ``
+        
+        if(layers[i].explosion) {             
+            objects += `
+                <div class="explosion" style="left: ${explosion.x}px;">
+                    <div class="fire-bottom-sm"></div>
+                    <div class="fire-bottom-md"></div>
+                    <div class="fire-bottom-lg"></div>
+                </div>
+            `;
+        }
+
+        renderExplosionDiv.innerHTML += `
+        <div class="layer-explosion"> <!-- layer explosion ${i} -->
+            ${objects}            
+        </div>
+        `
+    }
+    
+}
+
 // increment or decrement the score
 const scoreRender = () => {
     let scoreDiv = document.getElementById('score')
     scoreDiv.textContent = score
 }
+
 
 // move scope along the X axis
 const moveScope = (e) => {  
@@ -188,6 +202,7 @@ const moveScope = (e) => {
     // console.log(e.clientX)
 }
  
+
 // change rocket on the layers
 const shoot = (e) => {    
     if(e.code == 'Space' && !rocket.shoot) {
@@ -215,106 +230,74 @@ const shoot = (e) => {
     }
 }
 
-// Funcție pentru a verifica coliziunea
+
+// function to check collision
 const checkCollision = () => {
-    if (rocket.layer == ship.layer && Math.abs(ship.x - rocket.x) < 350) {
-        explosion.x = ship.x + 300;
+    if (rocket.layer == ship.layer && Math.abs(ship.x - rocket.x) < 200) {
         explosion.layer = ship.layer;
         layers[explosion.layer].explosion = true;
-        console.log("ship X is " + ship.x, "ex. X is " + explosion.x);
-        console.log("ship lay is " + ship.layer, "ex. lay is " + explosion.layer);
-        debugger
-
+        explosion.x = ship.x + 100;
+        
         clearInterval(timerRochet)
         rocket.shoot = false
         layers[rocket.layer].rochet = false
-        // layers[explosion.layer].explosion = false;
+        renderExplosion()
 
         score += 10;
         scoreRender();
 
-        resetShip();
-        return true; // Coliziune detectată
+        setTimeout(() => {
+            resetShip();
+        }, 3000);
+        return true; // Collision detected
     }
-    return false; // Nu există coliziune
+    return false; // There is no collision
 }
 
-/* const shoot = (e) => {    
-    if(e.code == 'Space' && !rocket.shoot) {
-        rocket.shoot = true 
-        rocket.layer = 0
-        rocket.x = scope.x + offset
-        layers[0].rochet = true
-        clearInterval(timerRochet) 
-
-        timerRochet = setInterval(() =>{
-
-            if(rocket.layer == 9) {
-                clearInterval(timerRochet)
-                rocket.shoot = false
-                layers[rocket.layer].rochet = false
-                score -= 10
-                scoreRender() 
-            } else {
-                layers[rocket.layer].rochet = false
-                rocket.layer ++
-                layers[rocket.layer].rochet = true
-
-                if (rocket.layer == ship.layer && Math.abs(ship.x - rocket.x) < 350) {
-                    // console.log( rocket.x, ship.x)
-                    // alert('boom!')  
-                    explosion.x = ship.x;  
-                    resetShip()  
-                    console.log("ship X is " + ship.x, "ex. X is" + explosion.x)
-                    console.log("ship lay is " + ship.layer, "ex. lay is" + explosion.layer)
-
-                    clearInterval(timerRochet)
-                    rocket.shoot = false
-                    layers[rocket.layer].rochet = false
-
-                    score += 10
-                    scoreRender()
-                }
-            }
-        }, 100 )
-    }
-}*/
 
 // change the direction and layer of the ship
-const resetShip = (i) => {
-    for (let i = 0; i <layers.length; i++) {
-        layers[i].ship = false
+const resetShip = () => {
+    for (let i = 0; i < layers.length; i++) {
+        layers[i].ship = false;       // reset to false all ship
+        layers[i].explosion = false;  // reset to false all explosion
     }
-    
-    ridx = Math.floor(Math.random() * 10)
-    ship.layer = ridx 
-    layers[ridx].ship = true
 
-    let rand = Math.random()
+    ridx = Math.floor(Math.random() * 10);
+    ship.layer = ridx;
+    layers[ridx].ship = true;
 
-    if ( rand >= 0.5 ) {
-        ship.x = layerWidth + 600
-        ship.dir = ship.dir
-    } else {        
-        ship.x = - 600
-        ship.dir = 1
+    let rand = Math.random();
+
+    if (rand >= 0.5) {
+        ship.x = layerWidth + 600;
+        ship.dir = ship.dir;
+    } else {
+        ship.x = -600;
+        ship.dir = 1;
     }
-    
-    clearInterval(timerShip)
+
+    clearInterval(timerShip);
+
     timerShip = setInterval(() => {
-        ship.x += 5  // ship.dir
+        if (layers[ridx].explosion) {
+            return;
+        }
 
-        if(ship.dir == 1 && ship.x > layerWidth + 600) {
-            resetShip()
+        ship.x += ship.dir;
+
+        if (ship.dir == 1 && ship.x > layerWidth + 600) {
+            resetShip();
         }
-        if(ship.dir == -1 && ship.x < -600) {
-            resetShip()
+        if (ship.dir == -1 && ship.x < -600) {
+            resetShip();
         }
-        render()
-    }, 1 )
+        render();
+    }, 1);
 }
+
 
 renderWave()
 resetShip()
+renderExplosion()
 render()
 scoreRender()
